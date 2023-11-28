8094bd9c93c9fa79132fcd7ac39d8163f838e4b0
jenish6ek <151835263+jenish6ek@users.noreply.github.com>
2023-11-28 15:54:08 +0600

    3th changes

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Privet iz Snnipetbox"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Otobrajenie zametki"))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Forma dlya sozdaniya novoi zametki..."))
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("Zapusk web-servera na http://127.0.0.1:8080")
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
 }
